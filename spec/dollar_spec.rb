638baf2bf7c9b1063e086378bc35ca095da0e489
Nestor Castillo <nestor.castillo@izea.com>
2014-12-27 12:59:22 -0500

    Chapter 2, adding test for changing interface

--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -4,14 +4,15 @@ describe Dollar do
   context '#multiplication' do
     it 'multiplies by 2' do
       five = Dollar.new(5)
-      five.times(2)
+      product = Dollar.new(five.times(2))
 
-      expect(five.amount).to eq 10
-      # weird side effect here.
-      # five is no longer 5, it is actually 10
-      # hence test fails!
-      five.times(3)
-      expect(five.amount).to eq 15
+      expect(product.amount).to eq 10
+
+      # expecting #times to return a new
+      # Dollar object here.
+      product = five.times(3)
+      # test fails with undefined method `amount' for 30:Fixnum
+      expect(product.amount).to eq 15
     end
   end
 end
