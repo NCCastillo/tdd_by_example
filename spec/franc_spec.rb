97327d466ad74af316ae8a43758e968e8f3a795d
Nestor Castillo <nestor.castillo@izea.com>
2014-12-27 16:43:21 -0500

    Chapter 5, duplicating test for Franc object.

---
+++ spec/franc_spec.rb
@@ -0,0 +1,12 @@
+require './lib/franc'
+
+describe Franc do
+  context '#times' do
+    it 'multiplies by 2 and 3' do
+      five = Franc.new(5)
+
+      expect(Franc.new(10)).to eq five.times(2)
+      expect(Franc.new(15)).to eq five.times(3)
+    end
+  end
+end
