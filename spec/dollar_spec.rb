bce3a51b5c1f8d5e5245bac636f521ba657f6e8b
Nestor Castillo <nestor.castillo@izea.com>
2014-12-27 14:20:50 -0500

    Chapter 3, test for checking equality

--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -1,7 +1,7 @@
 require './lib/dollar'
 
 describe Dollar do
-  context '#multiplication' do
+  context '#times' do
     it 'multiplies by 2 and 3' do
       five = Dollar.new(5)
       product = five.times(2)
@@ -11,6 +11,12 @@ describe Dollar do
       product = five.times(3)
 
       expect(product.amount).to eq 15
+    end
+  end
+
+  context 'equality' do
+    it 'checks for equality' do
+      expect(Dollar.new(5)).to eq Dollar.new(5)
     end
   end
 end
