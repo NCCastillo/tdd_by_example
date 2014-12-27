caa6bb720da3a6b2a23690af54ad86d8593656e3
Nestor Castillo <nestor.castillo@izea.com>
2014-12-27 14:57:16 -0500

    Chapter 4, rewriting expectation to compare Dollar to Dollar.

--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -5,11 +5,9 @@ describe Dollar do
     it 'multiplies by 2 and 3' do
       five = Dollar.new(5)
       product = five.times(2)
-
-      expect(product.amount).to eq 10
-
+      # rewriting our expectation to compare Dollars to Dollars
+      expect(Dollar.new(10)).to eq product
       product = five.times(3)
-
       expect(product.amount).to eq 15
     end
   end
