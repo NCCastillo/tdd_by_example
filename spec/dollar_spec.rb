a02f089cd606ec9ce97afd0189e63aa82d5f85bc
Nestor Castillo <nestor.castillo@izea.com>
2014-12-27 15:01:39 -0500

    Chapter 4, removing product variable from test.

--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -4,12 +4,9 @@ describe Dollar do
   context '#times' do
     it 'multiplies by 2 and 3' do
       five = Dollar.new(5)
-      product = five.times(2)
 
-      expect(Dollar.new(10)).to eq product
-      product = five.times(3)
-      # rewriting second expectation to compare Dollars to Dollars
-      expect(Dollar.new(15)).to eq product
+      expect(Dollar.new(10)).to eq five.times(2)
+      expect(Dollar.new(15)).to eq five.times(3)
     end
   end
 
