aa33a2aaef91091ea735aaffd1cf215a95d4e627
Nestor Castillo <nestor.castillo@izea.com>
2014-12-31 14:40:57 -0500

    Chapter 8, update all dollar tests to use Money.dollar

--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -7,16 +7,16 @@ describe Dollar do
     it 'multiplies by 2 and 3' do
       five = Money.dollar(5)
 
-      expect(Dollar.new(10)).to eq five.times(2)
-      expect(Dollar.new(15)).to eq five.times(3)
+      expect(Money.dollar(10)).to eq five.times(2)
+      expect(Money.dollar(15)).to eq five.times(3)
     end
   end
 
   context 'equality' do
     it 'checks for equality' do
-      expect(Dollar.new(5)).to eq Dollar.new(5)
-      expect(Dollar.new(5)).not_to eq Dollar.new(6)
-      expect(Dollar.new(5)).not_to eq Franc.new(5)
+      expect(Money.dollar(5)).to eq Money.dollar(5)
+      expect(Money.dollar(5)).not_to eq Money.dollar(6)
+      expect(Money.dollar(5)).not_to eq Franc.new(5)
     end
   end
 end
