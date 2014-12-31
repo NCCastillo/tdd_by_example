ed118763db482a1e6a4952dcdf8df800b8f18f96
Nestor Castillo <nestor.castillo@izea.com>
2014-12-31 14:45:35 -0500

    Chapter 8, update franc spec to reflect changes from Franc to Money.

--- spec/franc_spec.rb
+++ spec/franc_spec.rb
@@ -5,18 +5,18 @@ require './lib/dollar'
 describe Franc do
   context '#times' do
     it 'multiplies by 2 and 3' do
-      five = Franc.new(5)
+      five = Money.franc(5)
 
-      expect(Franc.new(10)).to eq five.times(2)
-      expect(Franc.new(15)).to eq five.times(3)
+      expect(Money.franc(10)).to eq five.times(2)
+      expect(Money.franc(15)).to eq five.times(3)
     end
   end
 
   context 'equality' do
     it 'checks for equality' do
-      expect(Franc.new(5)).to eq Franc.new(5)
-      expect(Franc.new(5)).not_to eq Franc.new(6)
-      expect(Franc.new(5)).not_to eq Dollar.new(5)
+      expect(Money.franc(5)).to eq Money.franc(5)
+      expect(Money.franc(5)).not_to eq Money.franc(6)
+      expect(Money.franc(5)).not_to eq Money.dollar(5)
     end
   end
 end
