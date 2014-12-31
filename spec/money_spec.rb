d038452f837452bf6df7c2f733a82dcca575a26e
Nestor Castillo <nestor.castillo@izea.com>
2014-12-31 15:04:56 -0500

    Consolidated tests into one Money test

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -2,13 +2,20 @@ require './lib/money'
 require './lib/dollar'
 require './lib/franc'
 
-describe Dollar do
+describe Money do
   context '#times' do
-    it 'multiplies by 2 and 3' do
+    it 'multiplies by 2 and 3 for Dollar' do
       five = Money.dollar(5)
 
       expect(Money.dollar(10)).to eq five.times(2)
       expect(Money.dollar(15)).to eq five.times(3)
+    end
+
+    it 'multiplies by 2 and 3 for Franc' do
+      five = Money.franc(5)
+
+      expect(Money.franc(10)).to eq five.times(2)
+      expect(Money.franc(15)).to eq five.times(3)
     end
   end
 
@@ -16,7 +23,9 @@ describe Dollar do
     it 'checks for equality' do
       expect(Money.dollar(5)).to eq Money.dollar(5)
       expect(Money.dollar(5)).not_to eq Money.dollar(6)
-      expect(Money.dollar(5)).not_to eq Money.franc(5)
+      expect(Money.franc(5)).to eq Money.franc(5)
+      expect(Money.franc(5)).not_to eq Money.franc(6)
+      expect(Money.franc(5)).not_to eq Money.dollar(5)
     end
   end
 end
