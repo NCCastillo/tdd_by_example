c16dc717d4ee45886eb7e3c0cb79e07e4ab2901b
Nestor Castillo <nestor.castillo@izea.com>
2015-01-01 10:57:58 -0500

    Chapter 12, add test for addition.

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -1,28 +1,27 @@
 require './lib/money'
 
 describe Money do
-  context '#times' do
-    it 'multiplies by 2 and 3 for Money' do
-      five = Money.dollar(5)
+  it 'multiplies by 2 and 3 for Money' do
+    five = Money.dollar(5)
 
-      expect(Money.dollar(10)).to eq five.times(2)
-      expect(Money.dollar(15)).to eq five.times(3)
-    end
+    expect(Money.dollar(10)).to eq five.times(2)
+    expect(Money.dollar(15)).to eq five.times(3)
   end
 
-  context 'equality' do
-    it 'checks for equality' do
-      expect(Money.dollar(5)).to eq Money.dollar(5)
-      expect(Money.dollar(5)).not_to eq Money.dollar(6)
-      expect(Money.franc(5)).not_to eq Money.dollar(5)
-    end
+  it 'checks for equality' do
+    expect(Money.dollar(5)).to eq Money.dollar(5)
+    expect(Money.dollar(5)).not_to eq Money.dollar(6)
+    expect(Money.franc(5)).not_to eq Money.dollar(5)
   end
 
-  context 'currency' do
-    it 'checks for currency' do
-      expect(Money.dollar(1).currency).to eq "USD"
-      expect(Money.franc(1).currency).to eq "CHF"
-    end
+  it 'checks for currency' do
+    expect(Money.dollar(1).currency).to eq "USD"
+    expect(Money.franc(1).currency).to eq "CHF"
   end
 
+  it 'does simple addition' do
+    sum = Money.dollar(5).plus(Money.dollar(5))
+
+    expect(Money.dollar(10)).to eq sum
+  end
 end
