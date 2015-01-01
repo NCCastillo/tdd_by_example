408177f85f184ad9ffd1551ec5674c98dbebc29a
Nestor Castillo <nestor.castillo@izea.com>
2015-01-01 14:34:32 -0500

    Chapter 14, adding rates to Bank. Skipping on using a Pair class.

--- lib/money.rb
+++ lib/money.rb
@@ -36,15 +36,23 @@ end
 
 class Bank
 
+  def initialize
+    @rates = {}
+  end
+
   def reduce(source, to)
     source.reduce(self, to)
   end
 
-  def add_rate(from, to, currency)
+  def add_rate(from, to, rate)
+    # skipping adding a Pair class as discussed in the book.
+    # Using an array pair here as the key instead.
+    # [from, to]
+    @rates[[from, to]] = rate
   end
 
   def rate(from, to)
-    (from == "CHF" && to == "USD") ? 2 : 1
+    @rates[[from, to]]
   end
 
 end
